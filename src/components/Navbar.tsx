499722b2c02404eb50a4c7d906f8edd5d3a96eb1
ayuugoyal <ayushgoyal8178@gmail.com>
2024-04-14 16:32:39 +0530

    feat: fixed all pages

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,15 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import { Home, LogOut, ListTodo, BadgeAlert, NotebookTabs } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import { log_out } from "@/actions/user";
+import { getUser, log_out } from "@/actions/user";
 import { toast } from "sonner";
+import { User } from "@/db/schema";
 
 const Navbar = () => {
+  const [userData, setUserData] = useState<User>();
   const loggingout = async () => {
     const rsponse: Boolean = await log_out();
     if (rsponse) {
@@ -15,6 +17,15 @@ const Navbar = () => {
       window.location.href = "/";
     } else toast.error("Error logging out");
   };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const user = await getUser();
+      console.log(user);
+      setUserData(user);
+    };
+    fetchData();
+  }, []);
   return (
     <div>
       <div className="flex flex-col w-56 p-5 gap-7 bg-[#a16207] text-white h-screen">
@@ -23,32 +34,44 @@ const Navbar = () => {
           <h1 className="font-semibold text-xl">ThriveAgro</h1>
         </div>
         <div className="flex flex-col justify-between h-full">
-          <div className="space-y-2">
-            <Link href={"/dashboard"}>
-              <div className="flex gap-2 hover:bg-[#ffbc6b] hover:text-black p-3 rounded-md cursor-pointer">
-                <Home />
-                Home
-              </div>
-            </Link>
-            <Link href={"/inventary"}>
-              <div className="flex gap-2 hover:bg-[#ffbc6b] hover:text-black p-3 rounded-md cursor-pointer">
-                <ListTodo />
-                My inventary
-              </div>
-            </Link>
-            <Link href={"/seva-sangh"}>
-              <div className="flex gap-2 hover:bg-[#ffbc6b] hover:text-black p-3 rounded-md cursor-pointer">
-                <BadgeAlert />
-                Report an issue
-              </div>
-            </Link>
-            <Link href={"/orders"}>
-              <div className="flex gap-2 hover:bg-[#ffbc6b] hover:text-black p-3 rounded-md cursor-pointer">
-                <NotebookTabs />
-                My orders
-              </div>
-            </Link>
-          </div>
+          {userData?.user_type === "seller" ? (
+            <div className="space-y-2">
+              <Link href={"/dashboard"}>
+                <div className="flex gap-2 hover:bg-[#ffbc6b] hover:text-black p-3 rounded-md cursor-pointer">
+                  <Home />
+                  Home
+                </div>
+              </Link>
+
+              <Link href={"/inventary"}>
+                <div className="flex gap-2 hover:bg-[#ffbc6b] hover:text-black p-3 rounded-md cursor-pointer">
+                  <ListTodo />
+                  My inventary
+                </div>
+              </Link>
+              <Link href={"/seva-sangh"}>
+                <div className="flex gap-2 hover:bg-[#ffbc6b] hover:text-black p-3 rounded-md cursor-pointer">
+                  <BadgeAlert />
+                  Report an issue
+                </div>
+              </Link>
+              <Link href={"/orders"}>
+                <div className="flex gap-2 hover:bg-[#ffbc6b] hover:text-black p-3 rounded-md cursor-pointer">
+                  <NotebookTabs />
+                  My orders
+                </div>
+              </Link>
+            </div>
+          ) : (
+            <div className="space-y-2">
+              <Link href={"/dashboard"}>
+                <div className="flex gap-2 hover:bg-[#ffbc6b] hover:text-black p-3 rounded-md cursor-pointer">
+                  <Home />
+                  Home
+                </div>
+              </Link>
+            </div>
+          )}
           <div>
             <Button
               className="hover:bg-[#FF0000] space-x-2 w-full bg-[#ffbc6b] text-black p-3 rounded-md cursor-pointer"
